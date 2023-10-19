f7f97532a24a28340e01345fef7bf664514763bf
rayaneYAJJOU <148465204+rayaneYAJJOU@users.noreply.github.com>
2023-10-19 17:16:59 +0200

    Delete centraletech_workshops-main directory

--- centraletech_workshops-main/workshop_1/exo1/ex.py
+++
@@ -1,9 +0,0 @@
-from random import random
-from math import pi
-
-N = int(1e6)
-P = [(random(),random()) for i in range(N)]
-C = [p for p in P if pow(p[0],2)+pow(p[1],2)<=1]
-PI = (4*len(C))/N
-
-print(abs(PI-pi))
